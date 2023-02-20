615c9c991bfbab3f8c4a5496782bf9619be461c3
lidiogomes <lidiogomes@gmail.com>
2023-02-20 00:10:40 -0300

    Adição figlet e yargs. Terminal help. Terminal commands. Lib Init.

--- index.js
+++ index.js
@@ -1,62 +1,30 @@
 ":" //#;exec /usr/bin/env node --input-type=module - "$@" < "$0"
-import { mouse, getActiveWindow, straightTo, Point, screen } from '@nut-tree/nut-js';
-import { type, speed } from './lib/initParams.js'
+import figlet from 'figlet';
+import { mouse, straightTo, Point, screen } from '@nut-tree/nut-js';
+import { logoOff, speed } from './lib/initParams.js'
 
 (async () => {
-    console.log(type, speed)
+
+    //Show logo
+    if (!logoOff) {
+
+        console.log(figlet.textSync('MouseJigglerJS'));
+        console.log(`Speed: ${speed}`)
+        
+    }
+
+    //Screen size
     const mainScreenWidth = await screen.width();
     const mainScreenHeight = await screen.height();
 
-    if (type !== 'random') {
+    setInterval(() => {
 
-        mouse.config.mouseSpeed = speed;
+        const randomWidth = parseInt(Math.random() * mainScreenWidth);
+        const randomHeight = parseInt(Math.random() * mainScreenHeight);
+        const newPoint = new Point(randomWidth, randomHeight);
 
-        for (let i = 0; i <= mainScreenWidth;) {
+        mouse.move(straightTo(newPoint));
 
-            let heightWay = i % 2;
+    }, speed);
 
-            if (!heightWay) {
-
-                for (let j = 0; j <= mainScreenHeight; j++) {
-
-                    const newPoint = new Point(i, j);
-
-                    await mouse.move(straightTo(newPoint));
-
-                }
-
-            } else {
-
-                for (let j = mainScreenHeight; j >= 0; j--) {
-
-                    const newPoint = new Point(i, j);
-
-                    await mouse.move(straightTo(newPoint));
-
-                }
-            }
-
-            //Infinity loop
-            ++i
-
-            if (i > mainScreenWidth) {
-
-                i = 0;
-
-            }
-
-        }
-
-    } else {
-
-        setInterval(() => {
-
-            const randomWidth = parseInt(Math.random() * mainScreenWidth);
-            const randomHeight = parseInt(Math.random() * mainScreenHeight);
-            const newPoint = new Point(randomWidth, randomHeight);
-
-            mouse.move(straightTo(newPoint));
-
-        }, speed);
-    }
 })();
